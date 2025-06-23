d26c07f60791e5839425bfed5770f4e26fa42155
kenankusu <kenan.kusu@gmail.com>
2025-06-23 14:14:49 +0200

    Merging von Änderungen in branch 'main'

--- lib/main.dart
+++ lib/main.dart
@@ -6,32 +6,9 @@ class MyFitnessApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
       title: 'streax',
       home: startseite(),
       debugShowCheckedModeBanner: false,
-=======
-      title: 'streax.',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 0, 13, 255)),
-      ),
-      home: const MyHomePage(title: 'streax.'),
->>>>>>> 0cbe17c1102e23e4b918ab6ff080833cc785df46
     );
   }
 }
