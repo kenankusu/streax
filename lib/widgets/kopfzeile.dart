f52dd10251e11e7586026f2eee143053b141f18e
KommaOS <hein.artur@icloud.com>
2025-06-25 16:30:41 +0200

    Kopfzeile entsprechend dem Mockup überarbeitet

--- lib/widgets/kopfzeile.dart
+++ lib/widgets/kopfzeile.dart
@@ -42,25 +42,29 @@ class _KopfzeileState extends State<Kopfzeile> {
   }
 
   Widget streakAnzeige(double progress) {
-    return SizedBox(
-      width: 200,
-      height: 100,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          CircularProgressIndicator(
-            value: progress, // Fortschrittswert wird übergeben
-            strokeWidth: 50,
-            valueColor: AlwaysStoppedAnimation<Color>(
-              const Color.fromARGB(255, 118, 3, 212),
+    return Transform.scale(
+      scale: 3.5,
+      child: SizedBox(
+        width: 100,
+        height: 100,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            CircularProgressIndicator(
+              value: progress,
+              strokeWidth: 3,
+              valueColor: AlwaysStoppedAnimation<Color>(
+                const Color.fromARGB(255, 255, 255, 0),
+              ),
+              backgroundColor: Colors.grey[800],
+              strokeCap: StrokeCap.round,
             ),
-            backgroundColor: Colors.grey[800],
-          ),
-          Text(
-            '${widget.streakWert}',
-            style: TextStyle(color: Colors.white, fontSize: 40),
-          ),
-        ],
+            Text(
+              '${widget.streakWert}',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -68,18 +72,29 @@ class _KopfzeileState extends State<Kopfzeile> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [Willkommen(), streakAnzeige(widget.streakWert / 30)],
+          children: [
+            Willkommen(),
+            Padding(
+              padding: const EdgeInsets.only(top: 20, right: 30),
+              child: streakAnzeige(widget.streakWert / 30),
+            ),
+          ],
         ),
-        SizedBox(height: 16),
-        Text(
-          zitat ?? "Zitat wird geladen...",
-          style: TextStyle(
-            color: Colors.grey[400],
-            fontStyle: FontStyle.italic,
+        SizedBox(height: 40),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Text(
+            zitat ??
+                "Fall in love with the process, and the results will come.",
+            style: Theme.of(context).textTheme.bodySmall?.copyWith(
+              fontStyle: FontStyle.italic,
+              color: Colors.grey[400],
+            ),
+            textAlign: TextAlign.center,
           ),
         ),
       ],
